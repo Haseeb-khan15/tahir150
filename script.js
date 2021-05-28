7e6b3c79cdb547f536955a757c550654f56c2111
Muhammad Tahir <tahirtv.islam@gmail.com>
2021-05-28 03:45:14 -0700

    fonts added

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function reload(){
 }
 
 
-var one = (n + " X " + 1 + " &nbsp; " + " = " + " &nbsp; " + + n*1)+"<br/>"
-var two = (n + " X " + 2 +  " &nbsp; " + " = " + " &nbsp; " + n*2)+"<br/>"
-var three = (n + " X " + 3 +  " &nbsp; " + " = " + " &nbsp; " + n*3)+"<br/>"
-var four = (n + " X " + 4 +  " &nbsp; " + " = " + " &nbsp; " + n*4)+"<br/>"
-var five = (n + " X " + 5 +  " &nbsp; " + " = " + " &nbsp; " + n*5)+"<br/>"
-var six = (n + " X " + 6 +  " &nbsp; " + " = " + " &nbsp; " + n*6)+"<br/>"
-var seven = (n + " X " + 7 +  " &nbsp; " + " = " + " &nbsp; " + n*7)+"<br/>"
-var eight = (n + " X " + 8 +  " &nbsp; " + " = " + " &nbsp; " + n*8)+"<br/>"
-var nine = (n + " X " + 9 +  " &nbsp; " + " = " + " &nbsp; " + n*9)+"<br/>"
-var ten = (n + " X " + 10 + " = " +  " &nbsp; " +  n*10)
+var one = (n + " x " + 1 + " &nbsp; " + " = " + " &nbsp; " + + n*1)+"<br/>"
+var two = (n + " x " + 2 +  " &nbsp; " + " = " + " &nbsp; " + n*2)+"<br/>"
+var three = (n + " x " + 3 +  " &nbsp; " + " = " + " &nbsp; " + n*3)+"<br/>"
+var four = (n + " x " + 4 +  " &nbsp; " + " = " + " &nbsp; " + n*4)+"<br/>"
+var five = (n + " x " + 5 +  " &nbsp; " + " = " + " &nbsp; " + n*5)+"<br/>"
+var six = (n + " x " + 6 +  " &nbsp; " + " = " + " &nbsp; " + n*6)+"<br/>"
+var seven = (n + " x " + 7 +  " &nbsp; " + " = " + " &nbsp; " + n*7)+"<br/>"
+var eight = (n + " x " + 8 +  " &nbsp; " + " = " + " &nbsp; " + n*8)+"<br/>"
+var nine = (n + " x " + 9 +  " &nbsp; " + " = " + " &nbsp; " + n*9)+"<br/>"
+var ten = (n + " x " + 10 + " = " +  " &nbsp; " +  n*10)
 
 Table = [one + two + three + four + five + six + seven + eight + nine + ten]
 document.getElementById("finalTable").innerHTML=Table

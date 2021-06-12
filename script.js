b8341567c181dfe00d86d61cf29db52a43368fb8
Muhammad Tahir <tahirtv.islam@gmail.com>
2021-06-12 00:35:52 -0700

    New pages Added

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.getElementById('input')
+let output = document.getElementById('output')
+
+function calculate(data){
+    if(data === "x"){
+        data = "*"
+    }
+    switch(data){
+         case 'AC': input.innerHTML="" , output.innerHTML="0";
+         break
+         case "C":
+            input.innerHTML = input.innerHTML.substr(0, input.innerHTML.length - 1);
+         break
+         case '=':
+             output.innerHTML = math.evaluate(input.innerText);
+        break
+         default : input.innerHTML += data;
+    }
+}
